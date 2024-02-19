bfd5636c708f5b9f12484fe5b3f17cde4a8023d4
Jessica Melo <jessicamelo2015@gmail.com>
2024-02-19 20:41:49 -0300

    exercicio

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import produtoPage from "../support/page_objects/produtos.page"
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -10,13 +12,51 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      produtoPage.visitarUrl()
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+     cy.fixture('produtos').then(dados =>{
+        produtoPage.buscarProduto(dados[0].nomeProduto)
+        produtoPage.adicionarNoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade
+        )
+     })
+     cy.fixture('produtos').then(dados =>{
+        produtoPage.buscarProduto(dados[1].nomeProduto)
+        produtoPage.adicionarNoCarrinho(
+            dados[1].tamanho,
+            dados[1].cor,
+            dados[1].quantidade
+        )
+     })
+     cy.fixture('produtos').then(dados =>{
+        produtoPage.buscarProduto(dados[2].nomeProduto)
+        produtoPage.adicionarNoCarrinho(
+            dados[2].tamanho,
+            dados[2].cor,
+            dados[2].quantidade
+        )
+     })
+     cy.fixture('produtos').then(dados =>{
+        produtoPage.buscarProduto(dados[3].nomeProduto)
+        produtoPage.adicionarNoCarrinho(
+            dados[3].tamanho,
+            dados[3].cor,
+            dados[3].quantidade
+        )
+     })
+    
+        produtoPage.finalizarCompra()
+    
+      
+    
+      
       
   });
-
+  
 
 })
---
+++ cypress/support/page_objects/produtos.page.js
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker';
+
+class ProdutoPage{
+visitarUrl(){
+    cy.visit('produtos')
+}
+
+
+buscarProduto(nomeProduto){
+    cy.get('[name = "s"]').eq(1).type(nomeProduto)
+    cy.get('.button-search').eq(1).click()
+}
+adicionarNoCarrinho(tamanho,cor,quantidade){
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-'+cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('.woocommerce-message').should('contain',' foi adicionado no seu carrinho.')
+}
+finalizarCompra(){
+    cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+    cy.get('#cart .buttons > .checkout').click()
+        cy.get('#billing_first_name').type(faker.person.firstName())
+        cy.get('#billing_last_name').type(faker.person.lastName())
+        cy.get('#billing_address_1').type('avenida colombo 597')
+        cy.get('#billing_city').type('colombo')
+        cy.get('#select2-billing_state-container').click().type('Paraná{enter}')
+        cy.get('#billing_postcode').type('86010-230')
+        cy.get('#billing_phone').type('41995588663')
+        cy.get('#billing_email').type(faker.internet.email())
+        cy.get('#payment_method_cod').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+
+        cy.get('.woocommerce-notice').should('contain','Obrigado. Seu pedido foi recebido.')
+    }
+
+}
+export default new ProdutoPage()
